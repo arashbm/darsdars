ab7b742b3b1f27cdf8766dc23acf372f3229e7d2
Arash Badie Modiri <arashbm@gmail.com>
2012-10-12 23:52:56 +0330

    added Student#weekday_sessions

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,4 +7,8 @@ class Student < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :course_ids
   has_many :student_courses
   has_many :courses, :through => :student_courses
+
+  def weekday_sessions(weekday)
+    Session.where(course_id: course_ids, weekday: weekday)
+  end
 end
