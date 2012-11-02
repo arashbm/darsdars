309ebd8991baef21871a7bf1f6a3c9bd819c9a78
Arash Badie Modiri <arashbm@gmail.com>
2012-11-02 15:16:09 +0330

    moved #on to Session

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,8 @@
 class Session < ActiveRecord::Base
   attr_accessible :course_id, :finish, :instructor, :session_type, :start, :weekday
   belongs_to :course
+
+  def self.on(date)
+    where(weekday: date.wday)
+  end
 end
